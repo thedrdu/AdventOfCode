19c0fbfc872075a99fbdc4294b5b5dfcb1a0f86d
thedrdu <thedrdu@gmail.com>
2022-05-27 23:53:08 -0400

    Day 2 + Day 3 Part 1

---
+++ Day2Part1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Author(s): thedrdu
+// Date: 05/27/2022
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+class Day1Part2Solver{
+    public static void Main(string[] args){
+
+        int distance = 0, depth = 0;
+        string[] command; //stores the command and corresponding value
+
+        using (StreamReader sr = new StreamReader("Day2_input.txt")){
+            string? line;
+            while((line = sr.ReadLine()) != null){
+                command = line.Split(' ');
+
+                switch(command[0]){
+                    case "forward":
+                        distance += Int32.Parse(command[1]);
+                        break;
+                    case "down":
+                        depth += Int32.Parse(command[1]);
+                        break;
+                    case "up":
+                        depth -= Int32.Parse(command[1]);
+                        break;
+                }
+
+            }
+        }
+        Console.WriteLine(distance * depth);
+    }
+}
---
+++ Day3Part1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Author(s): thedrdu
+// Date: 05/27/2022
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+class Day3Part1Solver{
+    public static void Main(string[] args){
+
+        int totalInputs = 0;
+        int[] occurrences = new int[12]; //NOTE: This length is hardcoded.
+
+        using (StreamReader sr = new StreamReader("Day3_input.txt")){
+            string? line;
+            while((line = sr.ReadLine()) != null){
+                for(int i = 0; i < line.Length; i++){
+                    if(line[i] == '1'){
+                        occurrences[i] += 1;
+                    }
+                }
+                totalInputs++;
+            }
+        }
+        /*
+        By dividing occurrences[i] by totalInputs, we can see if 0 or 1 is more common(threshold is totalInputs/2)
+        */
+        string gamma = "", epsilon = "";
+        foreach(int i in occurrences){
+            if(i >= totalInputs/2){ //1 majority
+                gamma += "1";
+                epsilon += "0";
+            }
+            else{
+                gamma += "0";
+                epsilon += "1";
+            }
+        }
+        int gammaInt = Convert.ToInt32(gamma, 2);
+        int epsilonInt = Convert.ToInt32(epsilon, 2);
+
+        Console.WriteLine(gammaInt * epsilonInt);
+    }
+}
